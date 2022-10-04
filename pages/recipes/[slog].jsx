669864150ac0ae15ee3ab562702805e53fd0e583
Moncef-Lak <76704813+Moncef-Lak@users.noreply.github.com>
2022-10-04 16:01:31 +0100

    fallback

--- pages/recipes/[slog].jsx
+++ pages/recipes/[slog].jsx
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Image from 'next/image'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -21,7 +22,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -39,8 +40,9 @@ export const getStaticProps = async ({ params }) => {
 }
 
 export default function RecipeDetails({ recipe }) {
-  const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
+  if(!recipe) return <Skeleton/>
 
+  const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
   return (
     <div>
       <div className="banner">
